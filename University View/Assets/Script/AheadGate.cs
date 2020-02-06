92860fac4013be123832cf6ccb95be5dedb881fd
Tushar <tushargpt224@gmail.com>
2020-02-06 23:41:57 +0530

    uni View

---
+++ University View/Assets/Script/AheadGate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AheadGate : MonoBehaviour
+{
+    public gaze obj;
+    public GameObject camera_rig;
+    public GameObject gateToAdd;
+    public Vector3 rotate;
+    public AudioClip audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj.imgGaze.fillAmount == 1)
+        {
+            camera_rig.transform.position = gateToAdd.transform.position;
+            camera_rig.transform.Rotate(rotate, Space.Self);
+            Invoke("play_audio", 0.5f);
+        }
+    }
+    void play_audio()
+    {
+        AudioSource.PlayClipAtPoint(audio, camera_rig.transform.position);
+    }
+}
---
+++ University View/Assets/Script/gaze.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gaze : MonoBehaviour
+{
+    
+    public GameObject camera_rig;
+    public Image imgGaze;
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera_rig.transform.Rotate(0, 45, 0);
+        imgGaze.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+
+    }
+
+    public void next_image()
+    {
+
+    }
+    public void GVRon()
+    {
+        gvrStatus = true;
+    }
+    public void GVRoff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+}
